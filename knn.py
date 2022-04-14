f050491ce214b4b207df6345a5c1d3909177187a
arod40 <arodacademic@gmail.com>
2022-04-14 16:34:06 -0500

    KNN interface

---
+++ knn.py
@@ -0,0 +1,15 @@
+class KNN:
+    def __init__(self, k):
+        pass
+
+    def _get_k_nearest_neigbors(X):
+        pass
+
+    def fit(X, y):
+        pass
+
+    def predict(X, y):
+        pass
+
+    def condense():
+        pass
