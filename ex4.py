02ec4257301fe694937099da3e93c9257ba6fcb9
arod40 <arodacademic@gmail.com>
2022-04-15 14:40:10 -0500

    Ex4 item d

--- ex4.py
+++ ex4.py
@@ -1,16 +1,24 @@
 import sys
 from pathlib import Path
+from random import seed
+from argon2 import verify_password
 
 import matplotlib.pyplot as plt
+from numpy.random import seed as np_seed
 from path import Path
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
+from tqdm import tqdm
 
 from data_utils import read_digits_data
 from knn import KNN
 from plot_utils import plot_train_and_test
 
+import numpy as np
+
 random_state = 0
+np_seed(9)
+seed(9)
 
 
 def evaluate(knn, X_train, X_test, y_train, y_test, verbose=False, interactive=False):
@@ -37,6 +45,30 @@ def evaluate(knn, X_train, X_test, y_train, y_test, verbose=False, interactive=F
     return Ein, Eout
 
 
+def experiment(X, y, verbose=False):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=500)
+    knn = KNN(3)
+    knn.fit(X_train, y_train)
+
+    Ein, Eout = evaluate(
+        knn, X_train, X_test, y_train, y_test, verbose=False, interactive=False
+    )
+
+    knn.condense(verbose=False)
+    Ein_condensed, Eout_condensed = evaluate(
+        knn, X_train, X_test, y_train, y_test, verbose=False, interactive=False
+    )
+
+    if verbose:
+        print("Full training data...")
+        print("Ein:", Ein, "Eout:", Eout)
+
+        print("Condensed data...")
+        print("Ein:", Ein_condensed, "Eout:", Eout_condensed)
+
+    return Ein, Eout, Ein_condensed, Eout_condensed
+
+
 if __name__ == "__main__":
     item = sys.argv[1]
 
@@ -50,7 +82,6 @@ if __name__ == "__main__":
     knn.fit(X_train, y_train)
 
     if item == "b":
-
         Ein, Eout = evaluate(
             knn, X_train, X_test, y_train, y_test, verbose=True, interactive=True
         )
@@ -60,4 +91,22 @@ if __name__ == "__main__":
         Ein, Eout = evaluate(
             knn, X_train, X_test, y_train, y_test, verbose=True, interactive=True
         )
+
+    if item == "d":
+        n = 1000
+        Eins, Eouts, Eins_condensed, Eouts_condensed = [], [], [], []
+        for _ in tqdm(range(n)):
+            Ein, Eout, Ein_condensed, Eout_condensed = experiment(X, y, verbose=True)
+            Eins.append(Ein)
+            Eouts.append(Eout)
+            Eins_condensed.append(Ein_condensed)
+            Eouts_condensed.append(Eout_condensed)
+
+        print("AVERAGE RESULTS")
+
+        print("Full training data...")
+        print("Ein:", np.mean(Eins), "Eout:", np.mean(Eouts))
+
+        print("Condensed data...")
+        print("Ein:", np.mean(Eins_condensed), "Eout:", np.mean(Eouts_condensed))
 
